e0f7d1fa3198e57cf28821a325044e4d7a51cbe2
kaw393939 <keith@webizly.com>
2013-08-08 23:54:34 +0000

    fixed email

--- form.php
+++ form.php
@@ -1,4 +1,6 @@
-<?php echo $_SERVER['REQUEST_METHOD']; ?>
+<?php echo 'Request method: ' . $_SERVER['REQUEST_METHOD']; ?>
+
+
 <FORM action="form.php" method="post">
     <fieldset>
     <LABEL for="firstname">First name: </LABEL>
@@ -6,11 +8,16 @@
     <LABEL for="lastname">Last name: </LABEL>
               <INPUT type="text" name="lastname" id="lastname"><BR>
     <LABEL for="email">email: </LABEL>
-              <INPUT type="text" id="email"><BR>
+              <INPUT type="text" name="email" id="email"><BR>
     <INPUT type="radio" name="sex" value="Male"> Male<BR>
     <INPUT type="radio" name="sex" value="Female"> Female<BR>
     <INPUT type="submit" value="Send"> <INPUT type="reset">
     </fieldset>
  </FORM>
 
-<?php print_r($_POST);?>
+<?php print_r($_POST);
+
+  //print_r($_SERVER);
+
+
+?>
