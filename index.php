be24811b4b576f3410e8dfe7b1349dc81b04c3d4
kaw393939 <keith@webizly.com>
2013-08-13 03:43:37 +0000

    fixed session adding and made it work as an object

--- index.php
+++ index.php
@@ -1,38 +1,30 @@
 <?php
   $program = new program();
-
-  if($_REQUEST == NULL) {
-    $program->homepage();
-  } elseif($_REQUEST['page'] == 'bankform') {
-
-     if($_SERVER['REQUEST_METHOD'] == 'GET') {
-       $program->bankform();
-     } else {
-       $transactions = new transactions();
-       if(array_key_exists('moretranstype', $_POST)) {
-         $transactions->addTransaction($_POST['type'],$_POST['amount'],$_POST['source']);
-         header('Location: http://www.mywebclass.org/gitexample/index.php?page=bankform');
-       } else {
-         $transactions->addTransaction($_POST['type'],$_POST['amount'],$_POST['source']);
-         print_r($_POST);
-         $transactions->printTransactions();
-       } 
-
-
-         print_r($transactions);
-     }    
-  }
-
-  
-//  $transactions->addTransaction('debit', 100, 'Walmart');
-//  $transactions->addTransaction('debit', 200, 'Target');
-
-//  $count = $transactions->countTransactions();
-
- // $transactions->printTransactions();
-
-
   class program {
+
+    public function __construct() {
+
+     if($_REQUEST == NULL) {
+       $this->homepage();
+     } elseif($_REQUEST['page'] == 'bankform') {
+        if($_SERVER['REQUEST_METHOD'] == 'GET') {
+          $this->bankform();
+        } else {
+          $transactions = new transactions();
+         if(array_key_exists('moretranstype', $_POST)) {
+           $transactions->addTransaction($_POST['type'],$_POST['amount'],$_POST['source']);
+           header('Location: http://www.mywebclass.org/gitexample/index.php?page=bankform');
+         } else {
+           $transactions->addTransaction($_POST['type'],$_POST['amount'],$_POST['source']);
+           $transactions->printTransactions();
+         } 
+       }   
+    }  
+
+
+
+
+    }
 
     function homepage() {
       
@@ -53,14 +45,17 @@
 
   class transactions {
     public $transactions = array();
+     public function __construct() {
 
+         session_start();
+
+     }
      public function addTransaction($type, $amount, $source) {
        $transaction = new transaction();
        $transaction->setType($type);
        $transaction->setAmount($amount);
        $transaction->setSource($source);
-
-       $this->transactions[] = $transaction;
+       $_SESSION['transactions'][] = $transaction;
      } 
      
      public function countTransactions() {
@@ -69,18 +64,17 @@
        return $count;
      }
     public function printTransactions() {
-      foreach($this->transactions as $transaction) {
-        echo $transaction->getType();
-        $transaction->printTransaction();
+      foreach($_SESSION['transactions'] as $transaction) {
+	$transaction->printTransaction();
       }
     }  
   
   }
 
   class transaction  {
-    private $type;
-    private $amount;
-    private $source;
+    public $type;
+    public $amount;
+    public $source;
    
 
     public function setType($type) {
@@ -102,9 +96,16 @@
     public function getSource() {
       return $this->source;
     }
+    public function getTransaction() {
+      $transaction = array();
+      $transaction['type'] = $this->type;
+      $transaction['amount'] = $this->amount;
+      $transaction['source'] = $this->source;
+      return $transaction;
+    }
     public function printTransaction() {
       echo '<hr>'; 
-      echo 'Transaction type: ' . $this->type . "<br> \n";
+      echo 'Transaction type: ' .   $this->type . "<br> \n";
       echo 'Transaction amount: ' . $this->amount . "<br> \n";
       echo 'Transaction source: ' . $this->source . "<br> \n"; 
 
