1a766ce52f05a7fe719988453554b174a3a20fcf
kaw393939 <keith@webizly.com>
2013-08-13 01:01:44 +0000

    fixed transaction

--- debitcredit.php
+++ debitcredit.php
@@ -1,35 +1,24 @@
 <?php
 
-  $obj = new debitcreditform();
-
-  //$obj->displayform(); 
-
-  $transaction = new transaction();
-  
-  $transaction->type = 'debit';
-  $transaction->amount = 100;
-  $transaction->source = 'Walmart';
-
   $transactions = new transactions();
-
-  $transactions->addTransaction($transaction);
-
-  $transaction->type = 'debit';
-  $transaction->amount = 200;
-  $transaction->source = 'Walmart';
   
-  $transactions->addTransaction($transaction);
+  $transactions->addTransaction('debit', 100, 'Walmart');
+  $transactions->addTransaction('debit', 200, 'Target');
 
+//  $count = $transactions->countTransactions();
 
-  $count = $transactions->countTransactions();
+  $transactions->printTransactions();
 
-  echo $count;
-  //print_r($transactions); 
 
   class transactions {
     public $transactions = array();
 
-     public function addTransaction($transaction) {
+     public function addTransaction($type, $amount, $source) {
+       $transaction = new transaction();
+       $transaction->setType($type);
+       $transaction->setAmount($amount);
+       $transaction->setSource($source);
+
        $this->transactions[] = $transaction;
      } 
      
@@ -38,13 +27,38 @@
 
        return $count;
      }
-
+    public function printTransactions() {
+      foreach($this->transactions as $transaction) {
+        $transaction->printTransaction();
+      }
+    }  
+  
   }
 
   class transaction  {
     public $type;
     public $amount;
     public $source;
+   
+
+    public function setType($type) {
+      $this->type = $type;
+    } 
+    public function setAmount($amount) {
+      $this->amount = $amount;
+    }
+    public function setSource($source) {
+      $this->source = $source;
+    }
+    
+    public function printTransaction() {
+      echo '<hr>'; 
+      echo 'Transaction type: ' . $this->type . "<br> \n";
+      echo 'Transaction amount: ' . $this->amount . "<br> \n";
+      echo 'Transaction source: ' . $this->source . "<br> \n"; 
+
+    }
+
   }
  
 
