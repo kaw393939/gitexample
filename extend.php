85a862761f4d9700b960ed5978a5d90dc13651bf
kaw393939 <keith@webizly.com>
2013-08-15 03:21:56 +0000

    added comments

--- extend.php
+++ extend.php
@@ -1,23 +1,29 @@
 <?php
+
+// instantiate the transaction
 $obj = new transactions();
-
+// look at transaction content
 print_r($obj);
-
+//define file operations
 class fileOperations {
+//public properties 
   public $filename = 'file.csv';
   public $directory = 'uploads';
-
+//constructor that is detecting parameters and setting to null, if they are not being passed when the object is created
   public function __construct($filename = NULL, $directory = NULL) {
+
+//testing to see if the parameter is null, if it is not null set the value
     if($filename != NULL) {
       $this->filename = $filename;
     }
+//another form of the above if statement
     if(!is_null($directory)) { 
       $this->directory = $directory;
     }
   }
 
 }
-
+// this is the transactions class and it has a parent class of file operations that is defined above.
 class transactions extends fileOperations {
 
 
--- typehint.php
+++ typehint.php
@@ -1,20 +1,20 @@
 <?php
-
+//This is an example of type hinting, if you change the name of "OtherClass" to somethign else and create that as an object, you will not be able to pass it into the MyClass method "test".  This is because the test method checks to see if the object being passed in is of the class it defines.
 $class1 = new MyClass;
-$class2 = new OtherClass;
+$class2 = new dog;
 
 $class1->test($class2);
 
 
 class MyClass {
-
- public function test(OtherClass $otherclass) {
+//"OtherClass" tells the function to only 
+ public function test(dog $otherclass) {
         echo $otherclass->var;
     }
 
 }
 
-class OtherClass {
+class dog {
     public $var = 'Hello World';
 }
 ?>
