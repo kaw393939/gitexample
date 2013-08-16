12c323935996e61b6bc217d27e92d3de19e5930a
kaw393939 <keith@webizly.com>
2013-08-16 01:23:34 +0000

    added csv reading with keys

--- file.php
+++ file.php
@@ -1,15 +1,23 @@
 <?php 
   
-$row = 1;
-if (($handle = fopen("exampe.csv", "r")) !== FALSE) {
+$first_run = TRUE;
+if (($handle = fopen("example.csv", "r")) !== FALSE) {
     while (($data = fgetcsv($handle, 0, ",")) !== FALSE) {
-       $records[] = $data;
-       
+       if($first_run == TRUE) {
+       	$field_names = $data;
+       	$first_run = FALSE;
+       } else {
+       	$data = array_combine($field_names, $data);
+       	$records[] = $data;
+       }
+      
     }
+    	     
+    
     
     fclose($handle);
-    
-    //print_r($records);
+    //print_r($field_names);
+    print_r($records);
 } else {
   echo 'no file found or some error'	;
 	
